7b6f2409b1630b68804b5c9bae7e52aa3c2197f3
Carlos Calderón Montes <carloscalderon1994@hotmail.com>
2016-08-26 20:52:30 -0500

    ...

--- app/src/main/java/com/facci/votantescacm/MainActivityCACM.java
+++ app/src/main/java/com/facci/votantescacm/MainActivityCACM.java
@@ -34,13 +34,29 @@ public class MainActivityCACM extends AppCompatActivity {
         txtRecinto = (EditText) findViewById(R.id.txtRecintoElectoral);
         txtAno = (EditText) findViewById(R.id.txtAnoNacimiento);
 
-        boolean estado = dbSQLITE.IngresarDatos(txtNombre.getText().toString(), txtApellido.getText().toString(), txtRecinto.getText().toString(), txtAno.getText().toString());
+        boolean estaInsertado = dbSQLITE.IngresarDatos(txtNombre.getText().toString(), txtApellido.getText().toString(), txtRecinto.getText().toString(), txtAno.getText().toString());
 
-        if (estado)
+        if (estaInsertado)
             Toast.makeText(MainActivityCACM.this,"Datos Ingresados correctamente",Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(MainActivityCACM.this,"Error de Ingreso",Toast.LENGTH_SHORT).show();
     }
 
+    public void verTodosClick(View v)
+    {
+        Cursor re = dbSQLITE.MostarTodos();
+        if (re.getCount()==0){
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+
+        while(re.moveToNext()){
+            buffer.append("Id : " + re.getString(0) + "\n");
+            buffer.append("Nombre : " + re.getString(1) + "\n");
+            buffer.append("Apellido : " + re.getString(2) + "\n");
+            buffer.append("Recinto : " + re.getString(3) + "\n");
+            buffer.append("Año nacimiento : " + re.getInt(4) + "\n\n");
+        }
+    }
 
 }
